588dbd3591a3f3d72fdbc76bc7b49dfe5ac3fc49
nhatshibaa <nhatntth2008069@fpt.edu.vn>
2021-10-23 22:20:49 +0700

    Add project files.

---
+++ MVCMovie/Controllers/WorldController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCMovie.Controllers
+{
+    public class WorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //public string Index()
+        //{
+        //    return "This is my default action...";
+        //}
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public IActionResult Welcome(string name, int id = 1)
+        {
+            ViewData["msg"] = "WELCOME" + name;
+            ViewData["ID"] = id;
+            return View();
+        }
+    }
+
+}
